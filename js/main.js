d43cf41bc7fe109df4b8b231f18bd3089de5c159
Кристина <kristina.shtaynmets@mail.ru>
2024-02-03 00:19:04 +0700

    Осуществили функционал кнопок и нормализовали работу кнопки редактирования во втором столбце

--- js/main.js
+++ js/main.js
@@ -6,35 +6,43 @@ new Vue({
                 title: '',
                 description: '',
                 deadline: '',
-                createdAt: new Date().toISOString().slice(0,10),
+                createdAt: new Date().toLocaleString(),
                 lastEdited: null
             },
             editedTask: null,
             editedTaskIndex: null,
+            editedColumn: null,
             plannedTasks: [],
             inProgressTasks: [],
+            testingTasks: [],
         }
     },
     methods:{
         addTask() {
             this.plannedTasks.push({...this.newTask});
-            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0,10), lastEdited: null };
+            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toLocaleString(), lastEdited: null };
         },
         deleteTask(taskIndex) {
             this.plannedTasks.splice(taskIndex, 1);
         },
-        startEditing(taskIndex) {
-            this.editedTask = {...this.plannedTasks[taskIndex]};
+        startEditing(taskIndex, column) {
+            this.editedTask = {...this[column][taskIndex]};
             this.editedTaskIndex = taskIndex;
+            this.editedColumn = column;
         },
         finishEditing(taskIndex) {
-            this.plannedTasks[taskIndex] = {...this.editedTask, lastEdited: new Date().toISOString().slice(0,10)};
+            this[this.editedColumn][taskIndex] = {...this.editedTask, lastEdited: new Date().toLocaleString()};
             this.editedTask = null;
             this.editedTaskIndex = null;
+            this.editedColumn = null;
         },
         moveToInProgress(taskIndex) {
             const taskToMove = this.plannedTasks.splice(taskIndex, 1)[0];
             this.inProgressTasks.push(taskToMove);
+        },
+        moveToTesting(taskIndex) {
+            const taskToMove = this.inProgressTasks.splice(taskIndex, 1)[0];
+            this.testingTasks.push(taskToMove);
         }
     }
 })
