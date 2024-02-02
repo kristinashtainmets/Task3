4c3d1b28fd62a304bbc4cbd4f7a5da9f706f537a
Кристина <kristina.shtaynmets@mail.ru>
2024-02-02 22:11:40 +0700

    Создали форму для создания карточки задачи

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-new vue({
+new Vue({
     el: '#app',
     data(){
-
+        return {
+            newTask: {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toISOString().slice(0,10)
+            },
+            tasks: []
+        }
     },
     methods:{
-        
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = { title: '', description: '', deadline: '', createdAt: new Date().toISOString().slice(0,10) };
+        }
     }
 })
